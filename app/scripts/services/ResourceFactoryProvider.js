10d660f9040442a0ee301c6180aebb2bb3cb1a28
mujeeb1238 <mujeeb1238@gmail.com>
2015-11-10 15:37:18 +0530

    feemaster addded

--- app/scripts/services/ResourceFactoryProvider.js
+++ app/scripts/services/ResourceFactoryProvider.js
@@ -313,7 +313,11 @@
           
           taxCalculationResource: defineResource(apiVer + "/loans/tax/:loanId", {}, {update: {method: 'PUT', params: {}}}),
           
-          calculationResource: defineResource(apiVer + "/loans/calculator", {}, {update: {method: 'PUT', params: {}}})
+          calculationResource: defineResource(apiVer + "/loans/calculator", {}, {update: {method: 'PUT', params: {}}}),
+          
+          feeMasterResource: defineResource(apiVer + "/feemaster/:id", {id:'@id'},{update: { method: 'PUT' }}),
+          
+          feeMasterTemplateResource: defineResource(apiVer + "/feemaster/template", {},{})
           
         };
       }];
